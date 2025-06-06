f3b4f6525e39bbd011e16f47b5846a09c9e51279
Pasquale Dazzeo <p.dazzeo@reply.it>
2025-06-06 15:02:49 +0200

    changed imports to avoid circular dependencies

--- backend/shared/models/__init__.py
+++ backend/shared/models/__init__.py
@@ -1,14 +1,8 @@
 from shared.models.base import BaseModel, UUIDMixin
 
-from modules.upload.models import Project, ProjectFile, UploadSession
-
-# Export all models for easy importing
 __all__ = [
     "BaseModel",
-    "UUIDMixin", 
-    "Project",
-    "ProjectFile",
-    "UploadSession"
+    "UUIDMixin"
 ]
 
 
